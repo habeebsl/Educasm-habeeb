2c0b5309847c1031f1da56887b69c03c87cea97d
habeeb <habeebomotolani2007@gmail.com>
2025-02-14 22:06:21 +0100

    Updated Initial UX

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
 import { ExploreView } from './components/Explore/ExploreView';
 import { PlaygroundView } from './components/Playground/PlaygroundView';
 import { ReviewMistakes } from './components/Review/ReviewMistakesView';
+import { Loading } from './components/shared/Loading';
 import { PreFillForm } from './components/shared/PreFillForm';
 import storageService from './services/storageService';
 import { Toaster, toast } from 'react-hot-toast';
 import { GoogleTagManager } from './components/shared/GoogleTagManager';
+import { UserContext } from './types';
 
 function App() {
-  const userContext = storageService.getUserInfo()
-
+  const [userContext, setUserContext] = useState<UserContext | null>(storageService.getUserInfo());
+  const [isLoading, setIsLoading] = useState(false)
   const handleError = (message: string) => {
     toast.error(message);
   };
@@ -19,10 +22,26 @@ function App() {
     toast.success(message);
   };
 
+  const handleSubmit = async (context: UserContext) => {
+    setIsLoading(true)
+    try {
+      storageService.saveUserInfo(context)
+      setUserContext(context)
+    } catch (error) {
+      console.error("Error saving user info:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   if (!userContext) {
-    return (
+    return isLoading ? (
+      <div className="flex justify-center py-12">
+        <Loading size="lg" />
+      </div>
+    ) : (
       <div className="min-h-screen bg-background text-white p-4">
-        <PreFillForm onSubmit={(context) => storageService.saveUserInfo(context) } />
+        <PreFillForm onSubmit={(context) => handleSubmit(context)} />
       </div>
     );
   }
